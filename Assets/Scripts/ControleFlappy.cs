31875028780800230026efbfc07c41ee4b040f50
Sau-Al <e2249485@cmaisonneuve.qc.ca>
2024-03-13 21:11:56 -0400

    Flappy Partie 4
    - Sons
    - Flappy animé

--- Assets/Scripts/ControleFlappy.cs
+++ Assets/Scripts/ControleFlappy.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class ControleFlappy : MonoBehaviour
 
@@ -13,21 +15,38 @@ public class ControleFlappy : MonoBehaviour
 
     public Sprite flappyBlesse;
     public Sprite flappyNormal;
+    public Sprite flappyBlesseFerme; // image de Flappy blessé volant
+    public Sprite flappyNormalFerme; // image de Flappy normal volant
     public GameObject objetPiece;
     public GameObject objetPack;
     public GameObject objetChampignon;
+
+    // Déclaration de la variable public de l’objet son
+    public AudioClip sonColonne;
+    public AudioClip sonOr;
+    public AudioClip sonPack;
+    public AudioClip sonChampignon;
+    public AudioClip sonFinPartie;
+
+    AudioSource sourceAudio; //Audio
+
+    public bool partieTerminee; // variable pour la fin de la partie
+    public bool flappyEtatBlesse; // variable pour l'état de Flappy
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sourceAudio = GetComponent<AudioSource>(); //va chercher le son
+        partieTerminee = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("right") || Input.GetKey("d"))
+        if (partieTerminee == false) {
+        
+         if (Input.GetKey("right") || Input.GetKey("d"))
         {
             vitesseX = 1;
 
@@ -49,7 +68,44 @@ public class ControleFlappy : MonoBehaviour
         else
         {
             vitesseY = GetComponent<Rigidbody2D>().velocity.y; //vitesse verticale actuelle
+        } }
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("up"))
+        {
+            if (flappyEtatBlesse == false)
+            {
+                // On change l'image de Flappy
+                GetComponent<SpriteRenderer>().sprite = flappyNormalFerme;
+            }
+            else
+            {
+                // On change l'image de Flappy
+                GetComponent<SpriteRenderer>().sprite = flappyBlesseFerme;
+            }
+
+            vitesseY = 5;
+
         }
+        else if (Input.GetKeyUp("w") || Input.GetKeyUp("up"))
+        {
+            if (flappyEtatBlesse == false)
+            {
+                // On change l'image de Flappy
+                GetComponent<SpriteRenderer>().sprite = flappyNormal;
+            }
+            else
+            {
+                // On change l'image de Flappy
+                GetComponent<SpriteRenderer>().sprite = flappyBlesse;
+            }
+        }
+        // Si aucune touche n'est enfoncée, on récupère la vélocité y actuelle
+        else
+        {
+
+            vitesseY = GetComponent<Rigidbody2D>().velocity.y;
+
+        }
+
 
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
@@ -60,7 +116,35 @@ public class ControleFlappy : MonoBehaviour
         //si l'objet touché est une colonne. alors on change l'image de Flappy change pour son image blessée.
         if (infocollision.gameObject.name == "Colonne")
         {
-            GetComponent<SpriteRenderer>().sprite = flappyBlesse;
+            sourceAudio.PlayOneShot(sonColonne, 1f); //Joue le clip qui se trouve dans la variable sonCol
+            
+            // On change l'état de Flappy à blessé
+            if (infocollision.gameObject.name == "Colonne" && flappyEtatBlesse == false)
+            {
+                // On change l'image de Flappy
+                GetComponent<SpriteRenderer>().sprite = flappyBlesse;
+                flappyEtatBlesse = true;
+
+            } else {
+
+                //Fin de partie est activée
+                partieTerminee = true;
+                
+                //Flappy peut maintenant tourner
+                GetComponent<Rigidbody2D>().freezeRotation = false;
+                //Vitesse angulaire
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5);
+                //Désactiver Collider2D
+                GetComponent<Collider2D>().enabled = false;
+
+                // Déclencher le son de la fin de la partie
+                sourceAudio.PlayOneShot(sonFinPartie, 1f);
+
+                //Relancer le jeu
+                Invoke("FinPartie", 3f);
+
+
+            }
         }
 
         //si l'objet touché est la pièce d'or. alors la pièce disparaît (il faut la désactiver).
@@ -70,6 +154,7 @@ public class ControleFlappy : MonoBehaviour
         {
             infocollision.gameObject.SetActive(false); //Pièce disparait
             Invoke("PieceRevient", 3f); //Pièce revient après 3 secondes
+            sourceAudio.PlayOneShot(sonOr, 1f); //Joue le clip qui se trouve dans la variable sonOr
         }
 
 
@@ -81,7 +166,7 @@ public class ControleFlappy : MonoBehaviour
             infocollision.gameObject.SetActive(false); //Pack de vie disparait
             Invoke("PackVieRevient", 3f); //Pack revient après 3 secondes
             GetComponent<SpriteRenderer>().sprite = flappyNormal; //Fappy redevient normal
-
+            sourceAudio.PlayOneShot(sonPack, 1f); //Joue le clip qui se trouve dans la variable sonPack
         }
 
 
@@ -92,6 +177,7 @@ public class ControleFlappy : MonoBehaviour
             infocollision.gameObject.SetActive(false); //Champignon disparait
             transform.localScale *= 1.5f; //Flappy grossit 
             Invoke("ChampignonRevient", 3f); //Champignon revient après 3 secondes
+            sourceAudio.PlayOneShot(sonChampignon, 1f); //Joue le clip qui se trouve dans la variable sonChampignon
         }
     }
 
@@ -113,5 +199,10 @@ public class ControleFlappy : MonoBehaviour
     {
         objetChampignon.SetActive(true);
         transform.localScale /= 1.5f;
+    }
+
+    void FinPartie()
+    {
+        SceneManager.LoadScene("Flappy");
     }
 }
